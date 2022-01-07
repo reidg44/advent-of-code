5d6f15b87b237facc3c638c9a5122ca9ba449702
Reid Geyer <geyer_reid@bah.com>
2022-01-07 13:22:36 -0500

    finish day04

--- 2021/Day_04/aoc_day_04.py
+++ 2021/Day_04/aoc_day_04.py
@@ -12,7 +12,7 @@ bingo_numbers = [int(x) for x in removed_lines[0].split(",")]
 removed_lines.pop(0)
 chunked_lines = list(chunked(removed_lines, 5))
 
-all_dfs = []
+all_dfs = dict()
 removed_cards = []
 removed_card_numbers = []
 
@@ -29,47 +29,49 @@ def remove_empty_vals(x):
     return [int(i) for i in x if i != ""]
 
 
-for chunk in chunked_lines:
+for bingo_card, chunk in enumerate(chunked_lines):
     integers = [x.split(" ") for x in chunk]
     integers = [remove_empty_vals(x) for x in integers]
-    all_dfs.append(pd.DataFrame(integers))
+    all_dfs[str(bingo_card)] = pd.DataFrame(integers)
 
 
 def check_for_bingo(bingo_number):
-
-    for bingo_card, df in enumerate(all_dfs):
+    cards_to_delete = []
+    for bingo_card, df in all_dfs.items():
         is_bingo = False
         if df.values.flatten().tolist().count("X") >= 5:
             for col in df.columns:
-                is_bingo = df[col].values.tolist() == bingo
+                if not is_bingo:
+                    is_bingo = df[col].values.tolist() == bingo
             for index, data in df.iterrows():
-                is_bingo = data.values.tolist() == bingo
+                if not is_bingo:
+                    is_bingo = data.values.tolist() == bingo
             if is_bingo:
                 got_bingo(bingo_card, df, bingo_number)
+                cards_to_delete.append(bingo_card)
                 pass
+    for card in cards_to_delete:
+        all_dfs.pop(card)
 
 
 def got_bingo(bingo_card, df, bingo_number):
     print(f"BINGO! Card: {bingo_card}")
     all_values = df.values.flatten().tolist()
     only_ints = sum(list(filter(lambda x: type(x) == int, all_values)))
-    if bingo_card not in removed_card_numbers:
-        removed_card_numbers.append(bingo_card)
-        removed_cards.append(df)
-    if len(removed_cards) == (len(all_dfs) - 1):
-        last_card = list(
-            set(range(len(all_dfs))).difference(set(removed_card_numbers)))[0]
-        all_values = all_dfs[last_card].values.flatten().tolist()
-        only_ints = sum(list(filter(lambda x: type(x) == int, all_values)))
-        print(f'Answer #2: {only_ints * bingo_number}')
+    # all_dfs.pop(bingo_card)
+    if len(all_dfs.items()) == 1:
+        for bingo_card, df in all_dfs.items():
+            all_values = df.values.flatten().tolist()
+            only_ints = sum(list(filter(lambda x: type(x) == int, all_values)))
+            print(f'Answer #2: {only_ints * bingo_number}')
         sys.exit()
 
 
 for bingo_number in bingo_numbers:
-    new_dfs = []
-    for df in all_dfs:
+    new_dfs = dict()
+    for bingo_card, df in all_dfs.items():
         new_df = df.replace(bingo_number, "X")
-        new_dfs.append(new_df)
+        new_dfs[bingo_card] = new_df
     all_dfs = new_dfs
     check_for_bingo(bingo_number)
 
